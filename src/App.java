bf8efbcb9946f9b92e00de0a21d4cc8d079d7004
Lukasz Ossowski <s33627@pjwstk.edu.pl>
2026-03-18 23:59:12 +0100

    func implementation

--- src/App.java
+++ src/App.java
@@ -1,9 +1,10 @@
 public class App {
 
-    public void printlol(){
-        System.out.println("lol");
-    }
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void printlol(){
+            System.out.println("lol");
+        }
+        public static void main(String[] args) throws Exception {
+            System.out.println("Hello, World!");
+            printlol();
     }
 }
