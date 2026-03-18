7888f065571508286c6cb910935705c4d8257d01
Lukasz Ossowski <s33627@pjwstk.edu.pl>
2026-03-18 23:52:07 +0100

    added new func in app

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
 public class App {
+
+    public void printlol(){
+        System.out.println("lol");
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
